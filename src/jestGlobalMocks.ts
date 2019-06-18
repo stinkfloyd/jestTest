5a4477ef69bd9bf5ff92b09eea782b90207b59fd
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2019-06-18 09:21:36 -0600

    "First set of standard tests passing"

---
+++ src/jestGlobalMocks.ts
@@ -0,0 +1,9 @@
+const mock = () => {
+  let storage = {};
+  return {
+    getItem: key => (key in storage ? storage[key] : null),
+    setItem: (key, value) => (storage[key] = value || ""),
+    removeItem: key => delete storage[key],
+    clear: () => (storage = {})
+  };
+};
